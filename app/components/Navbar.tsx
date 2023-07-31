b583875eb72c81d9c0dc40563340f8f587e536fa
ansman58 <javanslem@gmail.com>
2023-07-31 15:34:23 +0100

    web init

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import React from "react";
+import Logo from "../assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import clsx from "clsx";
+
+type Menu = { title: string; link: string };
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const menu = [
+    { title: "home", link: "/" },
+    { title: "about", link: "/about" },
+    { title: "department", link: "/department" },
+    { title: "doctors", link: "/doctors" },
+    { title: "blog", link: "/blog" },
+    { title: "contact", link: "/contact" },
+  ];
+  return (
+    <nav
+      className={clsx(
+        "flex  justify-between relative h-[4rem] px-[15px] py-[10px] items-center tablet:justify-between max-w-[70%] mx-auto"
+      )}
+    >
+      <Image
+        src={Logo}
+        alt="logo"
+        height={43}
+        width={178}
+        className="max-w-full"
+      />
+      <ul
+        className={clsx(
+          { ["hidden"]: !isMenuOpen },
+          "absolute top-[4rem] py-[14px] tablet:static tablet:top-0 tablet:flex tablet:items-center tablet:gap-3"
+        )}
+      >
+        {menu.map((item: Menu, index: number) => (
+          <li key={index} className="uppercase text-[14px] py-2">
+            <Link href={item.link}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <div
+        className="flex flex-col cursor-pointer tablet:hidden"
+        onClick={toggleMenu}
+      >
+        <div
+          className={clsx(
+            {
+              ["rotate-[-45deg] translate-y-[-5px] translate-x-[6px]"]:
+                isMenuOpen,
+            },
+            "w-[30px] h-[3px] bg-[red]  m-[6px_0] transition-transform "
+          )}
+        ></div>
+        <div
+          className={clsx(
+            { ["opacity-0"]: isMenuOpen },
+            "w-[30px] h-[3px] bg-[red]  m-[6px_0] transition-transform "
+          )}
+        ></div>
+        <div
+          className={clsx(
+            {
+              ["rotate-[45deg] translate-y-[5px] translate-x-[-6px]"]:
+                isMenuOpen,
+            },
+            "w-[30px] h-[3px] bg-[red]  m-[6px_0] transition-transform "
+          )}
+        ></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      baseColor:"#0051d2",
+      baseColorLight: "#ec473a",
+      dip: "#020a21",
+      heading: "#33363e",
+      pfont: "#797979",
+      white: "#fff",
+      bg_one: "#f7f7f7",
+      titleColor: "#2a2a2a",
+      border: "#e8edf1",
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -13,6 +24,15 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      mobile: "600px",
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
+    styles:{
+      
+    }
   },
   plugins: [],
 }
